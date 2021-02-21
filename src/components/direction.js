6d2946f4c645c91858b0e0c28a816e86f4738487
jnicewander <jnicewander@live.com>
2021-02-21 15:02:59 -0500

    Labels to show direction of shadow for each input.

--- src/components/direction.js
+++ src/components/direction.js
@@ -3,38 +3,50 @@ import React from 'react'
 const Direction = ({ direction, handleControlInputs }) => {
   return (
     <div aria-label="Shadow Direction">
-      <input
-        aria-label="Bottom Right" 
-        type="radio" 
-        name="direction" 
-        value="bottomRight"
-        checked={direction === "bottomRight"}
-        onChange={(e) => handleControlInputs(e)}
-      />
-      <input
-        aria-label="Bottom Left" 
-        type="radio" 
-        name="direction" 
-        value="bottomLeft"
-        checked={direction === "bottomLeft"}
-        onChange={(e) => handleControlInputs(e)}
-      />
-      <input
-        aria-label="Top Left" 
-        type="radio" 
-        name="direction" 
-        value="topLeft"
-        checked={direction === "topLeft"}
-        onChange={(e) => handleControlInputs(e)}
-      />
-      <input
-        aria-label="Top Right" 
-        type="radio" 
-        name="direction" 
-        value="topRight"
-        checked={direction === "topRight"}
-        onChange={(e) => handleControlInputs(e)}
-      />                  
+      <label>
+        A
+        <input
+          aria-label="Bottom Right" 
+          type="radio" 
+          name="direction" 
+          value="bottomRight"
+          checked={direction === "bottomRight"}
+          onChange={(e) => handleControlInputs(e)}
+        />
+      </label>
+      <label>
+        A
+        <input
+          aria-label="Bottom Left" 
+          type="radio" 
+          name="direction" 
+          value="bottomLeft"
+          checked={direction === "bottomLeft"}
+          onChange={(e) => handleControlInputs(e)}
+        />
+      </label>
+      <label>
+        A
+        <input
+          aria-label="Top Left" 
+          type="radio" 
+          name="direction" 
+          value="topLeft"
+          checked={direction === "topLeft"}
+          onChange={(e) => handleControlInputs(e)}
+        />
+      </label>
+      <label>
+        A
+        <input
+          aria-label="Top Right" 
+          type="radio" 
+          name="direction" 
+          value="topRight"
+          checked={direction === "topRight"}
+          onChange={(e) => handleControlInputs(e)}
+        />                  
+      </label>
     </div>
   )
 }
