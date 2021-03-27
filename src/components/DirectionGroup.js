68caced53548f4a82fe7572d79cb256ca5365343
jnicewander <jnicewander@live.com>
2021-03-27 12:01:25 -0400

    Initial styling complete.

--- src/components/DirectionGroup.js
+++ src/components/DirectionGroup.js
@@ -1,27 +1,40 @@
 import React from 'react'
+import { FaArrowCircleUp } from 'react-icons/fa'
+import './DirectionGroup.css'
 
 const DirectionGroup = ({ direction, setDirection }) => {
 
   const handleClick = (e) => {
     setDirection(e.target.value)
+    console.log(e.target.checked)
   }
 
   return (
-    <div aria-label="Shadow Direction">
-      <label>
-        A
+    <div
+      className="DirectionGroup"
+      aria-label="Shadow Direction"
+    >
+      <h2>Direction</h2>
+      <p>Control the shadow direction</p>
+      <label
+        className="DirectionLabel"
+      >        
         <input
+          className="DirectionInput"
           aria-label="Bottom Right" 
-          type="radio" 
+          type="radio"
           name="direction" 
           value="bottomRight"
           checked={direction === "bottomRight"}
           onChange={handleClick}
         />
+        <FaArrowCircleUp />
       </label>
-      <label>
-        A
+      <label
+        className="DirectionLabel"
+      >
         <input
+          className="DirectionInput"
           aria-label="Bottom Left" 
           type="radio" 
           name="direction" 
@@ -29,10 +42,13 @@ const DirectionGroup = ({ direction, setDirection }) => {
           checked={direction === "bottomLeft"}
           onChange={handleClick}
         />
+        <FaArrowCircleUp />
       </label>
-      <label>
-        A
+      <label
+        className="DirectionLabel"
+      >        
         <input
+          className="DirectionInput"
           aria-label="Top Left" 
           type="radio" 
           name="direction" 
@@ -40,17 +56,21 @@ const DirectionGroup = ({ direction, setDirection }) => {
           checked={direction === "topLeft"}
           onChange={handleClick}
         />
+        <FaArrowCircleUp />
       </label>
-      <label>
-        A
+      <label
+        className="DirectionLabel"
+      >        
         <input
+          className="DirectionInput"
           aria-label="Top Right" 
           type="radio" 
           name="direction" 
           value="topRight"
           checked={direction === "topRight"}
           onChange={handleClick}
-        />                  
+        />
+        <FaArrowCircleUp />                  
       </label>
     </div>
   )
