cbb5099c02a656fc1b14a6d11aca3b39caeba722
jnicewander <jnicewander@live.com>
2021-03-28 13:15:39 -0400

    Re-ordered direction buttons.

--- src/components/DirectionGroup.js
+++ src/components/DirectionGroup.js
@@ -6,7 +6,6 @@ const DirectionGroup = ({ direction, setDirection }) => {
 
   const handleClick = (e) => {
     setDirection(e.target.value)
-    console.log(e.target.checked)
   }
 
   return (
@@ -16,20 +15,7 @@ const DirectionGroup = ({ direction, setDirection }) => {
     >
       <h2>Direction</h2>
       <p>Control the shadow direction</p>
-      <label
-        className="DirectionLabel"
-      >        
-        <input
-          className="DirectionInput"
-          aria-label="Bottom Right" 
-          type="radio"
-          name="direction" 
-          value="bottomRight"
-          checked={direction === "bottomRight"}
-          onChange={handleClick}
-        />
-        <FaArrowUp />
-      </label>
+      
       <label
         className="DirectionLabel"
       >
@@ -49,20 +35,6 @@ const DirectionGroup = ({ direction, setDirection }) => {
       >        
         <input
           className="DirectionInput"
-          aria-label="Top Left" 
-          type="radio" 
-          name="direction" 
-          value="topLeft"
-          checked={direction === "topLeft"}
-          onChange={handleClick}
-        />
-        <FaArrowUp />
-      </label>
-      <label
-        className="DirectionLabel"
-      >        
-        <input
-          className="DirectionInput"
           aria-label="Top Right" 
           type="radio" 
           name="direction" 
@@ -72,6 +44,35 @@ const DirectionGroup = ({ direction, setDirection }) => {
         />
         <FaArrowUp />                  
       </label>
+      <label
+        className="DirectionLabel"
+      >        
+        <input
+          className="DirectionInput"
+          aria-label="Bottom Right" 
+          type="radio"
+          name="direction" 
+          value="bottomRight"
+          checked={direction === "bottomRight"}
+          onChange={handleClick}
+        />
+        <FaArrowUp />
+      </label>
+      <label
+        className="DirectionLabel"
+      >        
+        <input
+          className="DirectionInput"
+          aria-label="Top Left" 
+          type="radio" 
+          name="direction" 
+          value="topLeft"
+          checked={direction === "topLeft"}
+          onChange={handleClick}
+        />
+        <FaArrowUp />
+      </label>
+      
     </div>
   )
 }
