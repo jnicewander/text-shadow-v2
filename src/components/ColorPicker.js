1f7e9b413d9c42f079d8c6ad56c55bbe899b7710
jnicewander <jnicewander@live.com>
2021-03-29 18:33:08 -0400

    Add color picker component for controls.

---
+++ src/components/ColorPicker.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import { ChromePicker } from 'react-color'
+
+const ColorPicker = () => {
+  return (
+    <div>
+      <ChromePicker />
+    </div>
+  )
+}
+
+export default ColorPicker
