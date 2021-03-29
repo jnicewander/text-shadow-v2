f6f330710c7817b865c25922f0271dea41de6d53
jnicewander <jnicewander@live.com>
2021-03-29 18:46:31 -0400

    Add base functionality.

--- src/components/ColorPicker.js
+++ src/components/ColorPicker.js
@@ -1,10 +1,19 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { ChromePicker } from 'react-color'
 
 const ColorPicker = () => {
+  const [color, setColor] = useState({ hex: "#fff" })
+
+  const handleChange = (colorString) => {
+    setColor({hex: colorString})
+  }
+  console.log("Color: ", color)
   return (
     <div>
-      <ChromePicker />
+      <ChromePicker
+        color={color}
+        onChange={updatedColor => handleChange(updatedColor.hex)}
+      />
     </div>
   )
 }
