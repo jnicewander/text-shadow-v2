3e5d39e6ffd046d9faf07a12518c1a5d639991cd
jnicewander <jnicewander@live.com>
2021-04-04 13:54:22 -0400

    Add size component modal functionality.

--- src/components/SizeInput.js
+++ src/components/SizeInput.js
@@ -1,8 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import './SizeInput.css'
 
 const SizeInput = ({ size, controls, setControls, index }) => {
-
+  const [isOpen, setIsOpen] = useState(false)
+  
   const handleChange = (e) => {    
     const updatedControls = [...controls]
     updatedControls[e.target.dataset.index][e.target.name] = e.target.value
@@ -10,17 +11,32 @@ const SizeInput = ({ size, controls, setControls, index }) => {
   }
 
   return (
-    <input 
-      className={`SizeInput`}
-      aria-label="Size Input"
-      type="number"
-      name="size"
-      value={size}
-      min="1"
-      data-index={index}
-      onChange={e => handleChange(e)}
-      autoComplete="off"
-    />
+    <div>
+      <button
+        className={`SizeInput_Button`}
+        onClick={() => setIsOpen(!isOpen)}        
+      >
+        {size}
+      </button>
+      {
+        isOpen &&
+        <div
+          className={`SizeInput_Modal`}
+        >
+          <input 
+            className={`SizeInput`}
+            aria-label="Size Input"
+            type="number"
+            name="size"
+            value={size}
+            min="1"
+            data-index={index}
+            onChange={e => handleChange(e)}
+            autoComplete="off"            
+          />
+        </div>
+      }
+    </div>
   )
 }
 
