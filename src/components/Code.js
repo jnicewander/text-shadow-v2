d9c3c65c3c608a91bad1496a9c319c10e12f39b5
jnicewander <jnicewander@live.com>
2021-04-24 17:19:25 -0400

    Copy code to clipboard button added.

--- src/components/Code.js
+++ src/components/Code.js
@@ -1,16 +1,34 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { FaCopy } from 'react-icons/fa'
 import './Code.css'
 
 const Code = ({ result }) => {
+  const [isCopied, setIsCopied] = useState(false)
+
+  const handleCopy = e => {
+    e.preventDefault()
+    navigator.clipboard.writeText(`text-shadow: ${result};`)
+    setIsCopied(true)
+    setTimeout(() => {
+      setIsCopied(false)
+    }, 3000);
+  }
+  
   return (
     <div
-      className="Code_Container"
+      className="Code_Container"      
     >
-      <code
-        className="Code"
-      >
+      <code>
         <span>text-shadow:</span> {result};
       </code>
+      <button
+        onClick={e => handleCopy(e)}
+      >
+        <FaCopy />
+        {
+          isCopied ? "Copied!" : "Copy CSS"
+        }
+      </button>
     </div>
   )
 }
