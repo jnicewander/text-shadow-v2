d620bf3e36f8098da0da8c83357b859a1c458516
jnicewander <jnicewander@live.com>
2021-04-27 22:06:36 -0400

    Refactor to allow for non-diagonal directions.

--- src/utilities/build-string.js
+++ src/utilities/build-string.js
@@ -1,19 +1,40 @@
-function buildString(controlsArray, direction) {
-  let result = ``,
-      size = 1;
+function buildString(arrayOfControls, directionString) {
+  let resultArray = [];
+  let sizeTracker = 0;
 
-  controlsArray.forEach(control=> {
-    const horizontal = direction === 'bottomLeft' || direction === 'topLeft' ? '-' : ''
-    const vertical = direction === 'topLeft' || direction === 'topRight' ? '-' : ''
-
-    for (let i = 0; i < control.size; i++) {
-      result = result + `${horizontal}${size}px ${vertical}${size}px ${control.color}, `
-      size++;
+  arrayOfControls.forEach(control=> {
+    for (let i = 0; i < control.size; i++) {      
+      resultArray.push(cssString(directionString, sizeTracker + 1, control.color))
+      sizeTracker++;
     }
   })
-    
-  const formatResult = result.substring(0, result.length - 2)
-  return formatResult
+
+  const resultString = resultArray.join('');
+  const formatResult = resultString.substring(0, resultString.length - 2)
+  return formatResult;
+}
+
+function cssString(direction, size, color) {
+  switch (direction) {
+    case "top":
+      return `0px -${size}px ${color}, `
+    case "topRight":      
+      return `${size}px -${size}px ${color}, `
+    case "right":
+      return `${size}px 0px ${color}, `
+    case "bottomRight":
+      return `${size}px ${size}px ${color}, `
+    case "bottom":
+      return `0px ${size}px ${color}, `
+    case "bottomLeft":
+      return `-${size}px ${size}px ${color}, `
+    case "left":
+      return `-${size}px 0px ${color}, `
+    case "topLeft":
+      return `-${size}px -${size}px ${color}, `
+    default:
+      break;
+  }
 }
 
 export default buildString
