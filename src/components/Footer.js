ed903471469b1c297680b1958bc6aa156e6c1d73
jnicewander <jnicewander@live.com>
2021-04-27 22:08:24 -0400

    Update style to match header.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -4,7 +4,7 @@ import './Footer.css'
 const Footer = () => {
   return (
     <footer className="Footer_Container">
-      	<p>&copy; 2021 <a href="https://github.com/jnicewander">Justin Nicewander</a>, All Rights Reserved. <small>Feel free to steal all of this.</small></p>
+      	<p>&copy; 2021 <a href="https://github.com/jnicewander">Justin Nicewander</a>, All Rights Reserved.</p>
     </footer>
   )
 }
