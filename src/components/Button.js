44a279f65615258267e5d6a1ec07cae1fa221f34
Justin Nicewander <jnicewander@live.com>
2022-01-30 19:05:11 -0500

    Update class name imports.

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,12 +1,19 @@
 import React from 'react'
-import { baseBtn, addBtn, deleteBtn } from './Button.module.css'
+import styles from './Button.module.css'
 import { MdAddCircleOutline } from 'react-icons/md'
 import { FaTrash } from 'react-icons/fa'
 
-const Button = ({ className, children, handleClick = () => {}, ...props }) => {
+const Button = ({
+  className,
+  children,
+  type = 'button',
+  handleClick = () => {},
+  ...props
+}) => {
   return (
     <button
-      className={`${baseBtn} ${className}`}
+      className={`${styles.base} ${className}`}
+      type={type}
       onClick={(e) => handleClick(e)}
       {...props}>
       {children}
@@ -18,7 +25,7 @@ export default Button
 
 export const AddButton = (props) => {
   return (
-    <Button className={addBtn} aria-label='Add' {...props}>
+    <Button className={styles.add} aria-label='Add' {...props}>
       <MdAddCircleOutline />
     </Button>
   )
@@ -28,10 +35,18 @@ export const DeleteButton = ({ disabled = false, ...props }) => {
   return (
     <Button
       aria-label='Delete'
-      className={deleteBtn}
+      className={styles.delete}
       disabled={disabled}
       {...props}>
       <FaTrash />
+    </Button>
+  )
+}
+
+export const DirectionButton = ({ className, value, ...props }) => {
+  return (
+    <Button className={styles.direction} value={value} {...props}>
+      <span className={styles[value]}>TS</span>
     </Button>
   )
 }
